8a0965094cf3e1836ed3de48f511ba4ccbb114f4
svennergr <Svennergr@gmail.com>
2016-12-04 15:44:58 +0100

    Update bundle.js
    Updated Client-ID

--- bundle.js
+++ bundle.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
-var twitchStreams = require('twitch-get-stream')('i8zfh30sr6spknagwp0oj710oeo5s7q'); // twitch now ENFORCES client id usage apparently, so this is now required.
+var twitchStreams = require('twitch-get-stream')('jzkbprff40iqj646a697cyrvl0zt2m6'); // twitch now ENFORCES client id usage apparently, so this is now required.
 
 getStreamFromChannel = function(channel){
 	return twitchStreams.get(channel);
